f258000222079e1a943edba7ad3dd6c858468e6a
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-06-24 22:30:01 -0700

    Update model.py

--- model.py
+++ model.py
@@ -189,3 +189,30 @@ class FeedForward(nn.Module):
   def forward(self, xb):
     return self.layers(xb)
 
+
+class TransformerBlock(nn.Module):
+  def __init__(self, cfg):
+    super().__init__()
+    self.att = MultiHeadAttention(d_in=cfg["emb_dim"], 
+                                  d_out=cfg["emb_dim"], 
+                                  context_length=cfg["context_length"], 
+                                  dropout=cfg["drop_rate"], 
+                                  num_heads=cfg["n_heads"], 
+                                  qkv_bias=cfg["qkv_bias"])
+    self.ff = FeedForward(cfg)
+    self.norm1 = LayerNorm(cfg["emb_dim"])
+    self.norm2 = LayerNorm(cfg["emb_dim"])
+    self.drop_resid = nn.Dropout(cfg["drop_rate"])
+
+  def forward(self, x):
+    skip = x 
+    x = self.norm1(x)
+    x = self.att(x)
+    x = self.drop_resid(x)
+    x = x + skip 
+    skip = x
+    x = self.norm2(x)
+    x = self.ff(x)
+    x = self.drop_resid(x)
+    x = x + skip
+    return x
