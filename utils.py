f7c391b6313fcc756a87ca98b9ab6b7a638ddb13
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-07-28 01:14:16 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,6 +2,17 @@ import torch
 import torch.nn as nn
 
 
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 1024,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+
 def generate_text_simple(model, idx, max_new_tokens, context_size):
   for _ in range(max_new_tokens):
     idx_cond = idx[:, -context_size:]
