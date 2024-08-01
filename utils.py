c252f12be448aa411ae5600e0762b3c0dc3e4efc
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-07-31 22:46:14 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -23,3 +23,16 @@ def generate_text_simple(model, idx, max_new_tokens, context_size):
     idx_next = torch.argmax(probs, dim=-1, keepdim=True)
     idx = torch.cat((idx, idx_next), dim=-1)
   return idx
+
+
+def temperature_scaled_sampling(model, idx, max_new_tokens, context_size, temperature: float=1.0):
+  for _ in range(max_new_tokens):
+    idx_cond = idx[:, -context_size:]
+    with torch.no_grad():
+      logits = model(idx_cond)
+    logits = logits[:,-1, :] / temperature
+    probs = torch.softmax(logits, dim=-1)
+    idx_next = torch.multinomial(probs, num_samples=1)
+    idx = torch.cat((idx, idx_next), dim=-1)
+
+  return idx
