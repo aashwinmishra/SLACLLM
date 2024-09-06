b2ef4b3405e987e032b868a1a44c3ba9ffbecfef
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-09-06 00:51:05 -0700

    Update data_setup.py

--- data_setup.py
+++ data_setup.py
@@ -4,8 +4,41 @@ import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 import re
 import numpy as np
+import urllib.request
+import zipfile
+import os
+from pathlib import Path
 from typing import List, Dict, Tuple
 
+
+def get_spam_data(url: str="https://archive.ics.uci.edu/static/public/228/sms+spam+collection.zip", 
+                  zip_path: str="sms_spam_collection.zip", 
+                  extracted_path: str="sms_spam_collection", 
+                  data_file_path: str="SMSSpamCollection.tsv"):
+  """
+  Downloads dataset for Classification finetuning.
+  Args:
+    url: Location of zipfile with text
+    zip_path: Location to download zipfile
+    extracted_path: Drive to extract the zipfile contents
+    data_file_path: Filename to store data, located in extracted_path
+  """
+  data_file_path = Path(extracted_path)/data_file_path
+  if data_file_path.exists():
+    print(f"Data file {data_file_path} exists. Exiting...")
+    return 
+
+  with urllib.request.urlopen(url) as response:
+    with open(zip_path, "wb") as out_file:
+      out_file.write(response.read())
+
+  with zipfile.ZipFile(zip_path, "r") as zip_ref:
+    zip_ref.extractall(extracted_path)
+
+  original_file_path = Path(extracted_path) / "SMSSpamCollection"
+  os.rename(original_file_path, data_file_path) #C
+  print(f"File downloaded and saved as {data_file_path}")
+                    
 
 def load_data(file_path: str):
   with open(file_path, "r", encoding="utf-8") as file:
