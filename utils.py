4184892e58bf83801e84ecf17ea8c837115fa8b5
Aashwin Mishra <62309130+aashwinmishra@users.noreply.github.com>
2024-09-08 20:51:59 -0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,12 @@
 import torch
 import torch.nn as nn
 import tiktoken
+import os
+import urllib.request
+import json
+import numpy as np
+import tensorflow as tf
+from tqdm import tqdm
 
 
 GPT_CONFIG_124M = {
@@ -61,3 +67,78 @@ def top_k_sampling(model, idx, max_new_tokens, context_size, temperature: float=
     idx = torch.cat((idx, idx_next), dim=-1)
 
   return idx
+
+
+def download_and_load_gpt2(model_size, models_dir):
+    allowed_sizes = ("124M", "355M", "774M", "1558M")
+    if model_size not in allowed_sizes:
+        raise ValueError(f"Model size not in {allowed_sizes}")
+
+    model_dir = os.path.join(models_dir, model_size)
+    base_url = "https://openaipublic.blob.core.windows.net/gpt-2/models"
+    filenames = [
+        "checkpoint", "encoder.json", "hparams.json",
+        "model.ckpt.data-00000-of-00001", "model.ckpt.index",
+        "model.ckpt.meta", "vocab.bpe"
+    ]
+
+
+    os.makedirs(model_dir, exist_ok=True)
+    for filename in filenames:
+        file_url = os.path.join(base_url, model_size, filename)
+        file_path = os.path.join(model_dir, filename)
+        download_file(file_url, file_path)
+
+
+    tf_ckpt_path = tf.train.latest_checkpoint(model_dir)
+    settings = json.load(open(os.path.join(model_dir, "hparams.json")))
+    params = load_gpt2_params_from_tf_ckpt(tf_ckpt_path, settings)
+
+    return settings, params
+
+
+def download_file(url, destination):
+    try:
+        with urllib.request.urlopen(url) as response:
+            file_size = int(response.headers.get("Content-Length", 0))
+
+            if os.path.exists(destination):
+                file_size_local = os.path.getsize(destination)
+                if file_size == file_size_local:
+                    print(f"File already exists and is up-to-date: {destination}")
+                    return
+
+
+            block_size = 1024  # 1 Kilobyte
+            progress_bar_description = os.path.basename(url)  # Extract filename from URL
+            with tqdm(total=file_size, unit="iB", unit_scale=True, desc=progress_bar_description) as progress_bar:
+                # Open the destination file in binary write mode
+                with open(destination, "wb") as file:
+                    while True:
+                        chunk = response.read(block_size)
+                        if not chunk:
+                            break
+                        file.write(chunk)
+                        progress_bar.update(len(chunk))  # Update progress bar
+    except urllib.error.HTTPError:
+        s = (
+            f"The specified URL ({url}) is incorrect, the internet connection cannot be established,"
+            "\nor the requested file is temporarily unavailable.\nPlease visit the following website"
+            " for help: https://github.com/rasbt/LLMs-from-scratch/discussions/273")
+        print(s)
+
+
+def load_gpt2_params_from_tf_ckpt(ckpt_path, settings):
+    params = {"blocks": [{} for _ in range(settings["n_layer"])]}
+    for name, _ in tf.train.list_variables(ckpt_path):
+        variable_array = np.squeeze(tf.train.load_variable(ckpt_path, name))
+        variable_name_parts = name.split("/")[1:]  
+        target_dict = params
+        if variable_name_parts[0].startswith("h"):
+            layer_number = int(variable_name_parts[0][1:])
+            target_dict = params["blocks"][layer_number]
+        for key in variable_name_parts[1:-1]:
+            target_dict = target_dict.setdefault(key, {})
+        last_key = variable_name_parts[-1]
+        target_dict[last_key] = variable_array
+    return params
